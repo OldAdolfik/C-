f223913747ab122a96a9692151ab3fde5d9e18de
Stepan <apollo.black166@gmail.com>
2022-09-05 15:07:48 +0300

    Added negative numbers

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -10,7 +10,16 @@ while (num1>=110 && num1<1000)
         num1 = num1-10;
     Console.WriteLine(num1);
 }
-if (num2<100 || num2>999)
+while (num1<=-110 && num1>-1000)
+{
+    num1 = num1+110;
+
+    while (num1<=-10)
+
+        num1 = num1+10;
+    Console.WriteLine(-num1);
+}
+if ((num2<100 && num2>-100) || (num2>999) || (num2<-999))
 {
     Console.WriteLine("Это не трёхзначное число =)");
 }
