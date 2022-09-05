1c1a8c2d293adfde4f1728915d38451fcbdca872
Stepan <apollo.black166@gmail.com>
2022-09-05 12:59:26 +0300

    Решение через массив 2 задания 1 семинара

---
+++ Task2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int day = int.Parse(Console.ReadLine());
+string[] array = new string[] {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+if (day>0 && day<8)
+{
+    Console.WriteLine(array[day-1]);
+}
+else
+{
+    Console.WriteLine("Такого дня не существует");
+}
