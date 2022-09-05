6e6e63e3e70a66c0a01c5dda55cf9bbed6467f5d
Stepan <apollo.black166@gmail.com>
2022-09-05 12:34:26 +0300

    Complete Task2 about number day and name of the day

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 == 1)
+{
+    Console.WriteLine("Это понедельник");
+}
+if (num1 == 2)
+{
+    Console.WriteLine("Это вторник");
+}
+if (num1 == 3)
+{
+    Console.WriteLine("Это среда");
+}
+if (num1 == 4)
+{
+    Console.WriteLine("Это четверг");
+}
+if (num1 == 5)
+{
+    Console.WriteLine("Это пятница");
+}
+if (num1 == 6)
+{
+    Console.WriteLine("Это суббота");
+}
+if (num1 == 7)
+{
+    Console.WriteLine("Это воскресенье");
+}
+else
+{
+    Console.WriteLine("Такого дня не существует");
+}
