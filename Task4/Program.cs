206ed6646be9e655d63c81a3d4b08c862afdc56e
Stepan <apollo.black166@gmail.com>
2022-09-05 14:24:13 +0300

    Создал программу без помощи

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = num1;
+while (num1>=110 && num1<1000)
+{
+    num1 = num1-110;
+
+    while (num1>=10)
+
+        num1 = num1-10;
+    Console.WriteLine(num1);
+}
+if (num2<100 || num2>999)
+{
+    Console.WriteLine("Это не трёхзначное число =)");
+}
