0cf21c71a62eb83841f88deb912d6f536b627d50
Stepan <apollo.black166@gmail.com>
2022-09-06 17:57:58 +0300

    Completed the Task1 on sem2

--- Task2.1/Program.cs
+++ Task2.1/Program.cs
@@ -1,11 +1,16 @@
-﻿Console.WriteLine("Введите номер дня недели");
-int day = int.Parse(Console.ReadLine());
-string[] array = new string[] {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
-if (day>0 && day<8)
+﻿Console.WriteLine("Введите двухзначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1/10;
+int num3 = num1%10;
+if (num1<10 || num1>99)
 {
-    Console.WriteLine(array[day-1]);
+    Console.WriteLine("Не обманывай меня :)");
+}
+else if (num2<num3)
+{
+    Console.WriteLine(num3 + " - наибольшая цифра");
 }
 else
 {
-    Console.WriteLine("Такого дня не существует");
+    Console.WriteLine(num2 + " - наибольшая цифра");
 }
