9726d386ba295d683e63c34ca63d60c9021932ff
Stepan <apollo.black166@gmail.com>
2022-09-06 19:19:53 +0300

    Улучшили наше решение в первом задании добавлением механизма случайного числа

---
+++ Task2.1.1/Program.cs
@@ -0,0 +1,5 @@
+﻿int num0 = new Random().Next(100,1000);  //[100...999]
+Console.WriteLine("Случайное число: " + num0);
+int num1 = num0/100;
+int num3 = num0%10;
+Console.WriteLine("Крайняя левая и крайняя правая цифры: " + num1 + "" + num3);
