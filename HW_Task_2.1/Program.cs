bf7c2288743dbc49ba61944dc70b33fbfc0dc0f8
Stepan <apollo.black166@gmail.com>
2022-09-06 19:46:12 +0300

    Completed the task1 (HW2)

---
+++ HW_Task_2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num0 = Convert.ToInt32(Console.ReadLine());
+int num2 = num0%100;
+num2 = num2/10;
+if (num0>99 && num0<1000)
+{
+Console.WriteLine(num2);
+}
+else if (num0<-99 && num0>-1000)
+{
+    num2 = -num2;
+    Console.WriteLine(num2);
+}
+else Console.WriteLine("Я просил трёхзначное число :)");
