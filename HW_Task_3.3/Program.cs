e9b65329e29a82977369c2fce2e2680309440307
Stepan <apollo.black166@gmail.com>
2022-09-07 17:34:55 +0300

    Completed the Task3.3 (HW3)

---
+++ HW_Task_3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 1;
+if (num1>0)
+{
+    while (num2<=num1)
+    {
+    Console.WriteLine(num2*num2*num2);
+    num2++;
+    }
+}
+else if (num1<0)
+{
+    num2=-num2;
+    while (num2>=num1)
+    {
+        Console.WriteLine(num2*num2*num2);
+        num2--;
+    }
+}
+else Console.WriteLine("1, 0");
