8a476e228ab3276d14bca5e546022ac1eb1ecc07
Stepan <apollo.black166@gmail.com>
2022-09-10 16:46:56 +0300

    Completed the Task4.3 (HW4)

---
+++ HW_Task_4.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] arrayRanElem(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i]=new Random().Next(0, 2147483647);
+    }
+    return(array);
+}
+int[] result = arrayRanElem(8);
+Console.WriteLine($"Массив из 8 случанйх элементов: [{String.Join(",", result)}]");
