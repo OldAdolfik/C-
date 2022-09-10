05975fd2d4cd5bd129770bf480e51618e7aed87b
Stepan <apollo.black166@gmail.com>
2022-09-10 16:25:29 +0300

    Completed the Task4.2 (HW4)

---
+++ HW_Task_4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigNum(num1));
+int SumDigNum(int end)
+{
+    int result=0;
+    while (end>0)
+    {
+        result=result + end%10;
+        end=end/10;
+    }
+    return(result);
+}
