53c3bc992da006ff3083e6b34c5b39294a790e7e
Stepan <apollo.black166@gmail.com>
2022-09-10 14:51:12 +0300

    Completed the task4.4

---
+++ Task4.4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] BinArray (int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+    return array;
+}
+int [] resultArray = BinArray(8);
+Console.WriteLine($"ResultArray: [{String.Join(",", resultArray)}]");
