d9e0da3304048793f96f84ff5092dbd6f2410abe
Stepan <apollo.black166@gmail.com>
2022-09-10 14:19:31 +0300

    Completed the Task 4.3

---
+++ Task4.3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(MultOfNumbers(num1));
+int MultOfNumbers (int end)
+{
+    int mult=1;
+    if (end>0)
+    {
+        for (int i = 1; i <= end; i++)
+        {
+            mult=mult*i;
+        }
+        return(mult);
+    }
+    else if (end<0)
+    {
+        for (int i = -1; i >= end; i--)
+        {
+            mult=mult*i;
+        }
+        return(mult);
+    }
+    else 
+    {
+        int zero=0;
+        return(zero);
+    }
+}
