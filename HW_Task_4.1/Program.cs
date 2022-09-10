db96dcf8f23cb0efc474f544d4de44ea97f8da64
Stepan <apollo.black166@gmail.com>
2022-09-10 16:02:49 +0300

    Completed the Task4.1 (HW4)

---
+++ HW_Task_4.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.WriteLine("Я умею возводить число A в натуральную степень B.");
+Console.WriteLine("Введите число A: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(exponent(num1,num2));
+double exponent (double A, int B)
+{
+    int hz = B;
+    while(hz==B)
+    {
+        A=Math.Pow(A,B);
+        hz+=1;
+    }
+    return(A);
+}
+
+// ПОЯСНЕНИЕ. Не совсем понял зачем здесь цикл, поэтому придумал обозначение переменной hz, которая всем понятна, и сделал псевдоцикл.
