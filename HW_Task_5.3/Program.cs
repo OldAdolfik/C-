2cf0c2c7d28813cd81608b33a25ad342ed42f8b5
Stepan <apollo.black166@gmail.com>
2022-09-14 17:23:49 +0300

    completed the task5.3 (hw5)

---
+++ HW_Task_5.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int size = new Random().Next(2, 11);
+double max = -1000000000000;
+double min = 1000000000000;
+double[] array = new double [size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    if (array[i]>max) max = array[i];
+    if (array[i]<min) min = array[i];
+}
+Console.WriteLine($"[{String.Join(" ", array)}] -> {max-min}");
