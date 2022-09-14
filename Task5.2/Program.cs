d5864ceeff3d76cfac65bc2c81b04f21d89aa90c
Stepan <apollo.black166@gmail.com>
2022-09-14 16:09:25 +0300

    completed the task5.2

---
+++ Task5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int size = new Random().Next(10);                          //Можно ставить любой размер массива
+int[] array = new int [size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next();
+}
+Console.Write($"[{String.Join(",", array)}] ->");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i]*(-1);
+}
+Console.Write($" [{String.Join(",", array)}]");
