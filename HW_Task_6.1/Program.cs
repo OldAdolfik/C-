142bd3f92a0bd2d5a3630821259610e8cc3d572f
Stepan <apollo.black166@gmail.com>
2022-09-19 15:03:50 +0300

    Completed the Task6.1 (HW6)

---
+++ HW_Task_6.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел: ");
+int [] num = Array.ConvertAll(Console.ReadLine().Split(),Convert.ToInt32);
+int count = 0;
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i]>0) count++;
+}
+Console.WriteLine(count);
