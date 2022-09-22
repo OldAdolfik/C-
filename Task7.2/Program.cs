63a08f195a89f3aa26d42628a1fbd4432d6800fc
Stepan <apollo.black166@gmail.com>
2022-09-22 15:35:17 +0300

    completed the task7.2

---
+++ Task7.2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.WriteLine("Введите количество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Массив:");
+int[,] GetArray(int m, int n)
+{
+    int[,] Matrix = new int[m, n];
+    for (int i = 0; i < Matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < Matrix.GetLength(1); j++)
+        {
+            Matrix[i, j] = i + j;
+        }
+    }
+    return Matrix;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetArray(m, n);
+PrintArray(resultMatrix);
