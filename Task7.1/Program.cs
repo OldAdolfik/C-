c9051121718aac765995edd778eca2f287015550
Stepan <apollo.black166@gmail.com>
2022-09-22 15:04:48 +0300

    completed the task7.1

---
+++ Task7.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(11);
+        }
+    }
+    return (matrix);
+}
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetMatrix(3, 4);
+PrintMatrix(resultMatrix);
